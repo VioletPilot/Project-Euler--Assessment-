3cc2f9afd4e7bcd20425e0a129102e8f07c606c5
VioletPilot <sherwin.gopalkista06@gmail.com>
2024-04-26 07:24:19 +0200

    Nav bar with burger menu when too small. Scripts for Q1&2

--- scripts-Q1.js
+++ scripts-Q1.js
@@ -1,1 +1,31 @@
 //Script for the 1st problem
+
+$(document).ready(function() 
+{
+//Wait for document to completely render
+
+        const button_compute_q1 = document.querySelector("#test"/*'#compute-q1'*/);
+        button_compute_q1.addEventListener("click", function() {
+            // Call your function here
+            multiples_under_max();
+        });
+
+        function multiples_under_max()
+        {
+            const multiple1    = 3;//document.querySelector("#1st-multiple");
+            const multiple2    = 5;//document.querySelector("#2nd-multiple");
+            const max          = 1000;//document.querySelector("#maximum");
+
+            var total = 0;
+
+            for (i=0; i<max; i++)
+            {
+                if (((i%multiple1===0)||(i%multiple2===0)))
+                {
+                    total+=i;
+                }
+            }
+            button_compute_q1.innerText=total;
+        }
+//End of document ready
+});
--- scripts-Q2.js
+++ scripts-Q2.js
@@ -1,1 +1,32 @@
 //Script for the 2nd problem
+
+$(document).ready(function() {
+    //Wait for document to completely render
+        const button_compute_q1=document.querySelector('#test'/*'#compute-q2'*/);
+        button_compute_q1.addEventListener("click", function() {
+            // Call your function here
+            fib_even_sum();
+        });
+
+        function fib_even_sum()
+        {
+            var fib1 = 0;
+            var fib2 = 1;
+            var sum_even=0;
+
+            for(fib3=0; fib3<4000000;)
+            {
+                fib3 = fib1+fib2;
+                fib1 = fib2;
+                fib2 = fib3;
+
+                if ((fib3%2)===0)
+                {
+                    sum_even+=fib3;
+                }
+            }
+            button_compute_q1.innerText=sum_even;
+        }
+    
+    //End of document ready
+    });
